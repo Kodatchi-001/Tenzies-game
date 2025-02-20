c8d7984815242224e305ee40b10f77a946719973
Ahmed Hariri <Ahmedhariri58@gmail.com>
2025-02-20 23:11:06 +0100

    Add game timer and score panel with Navbar, Confetti effect, and Game Timer

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -4,7 +4,7 @@ import Die from "../die";
 import Hero from "../hero";
 import { v4 as uuidv4 } from "uuid";
 import { diceTypes } from "@/types";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Confetti from "react-confetti"
 import { useWindowSize } from 'react-use'
 import Score from "../score";
@@ -17,6 +17,10 @@ export default function Game() {
     const gameWon: boolean = dice?.every((die) => die?.isHeld) && dice?.every((die) => die?.value === dice[0]?.value);
     const { width, height } = useWindowSize()
     const [isClient, setIsClient] = useState<boolean>(false);
+    const [timer, setTimer] = useState<string>('');
+    const [timerRuns, setTimerRuns] = useState<boolean>(false);
+    const [time, setTime] = useState<{ seconds: number, minutes: number }>({ seconds: 0, minutes: 0 });
+    const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
     /*---> Functions <---*/
     const generateAllDies = (): diceTypes[] => {
@@ -33,7 +37,30 @@ export default function Game() {
     }
     const hold = (id: string): void => {
         setDice((prevState) => prevState?.map((die) => die?.id === id ? { ...die, isHeld: !die?.isHeld } : die));
+        gameWon ? setTimerRuns(false) : setTimerRuns(true)
     }
+    const gameTimer = (): void => {
+        if (timerRuns) {
+            if (intervalRef.current) { clearInterval(intervalRef.current) }
+            intervalRef.current = setInterval(() => {
+                setTime((prevState) => {
+                    let newSeconds = prevState?.seconds < 59 ? prevState?.seconds + 1 : 0;
+                    let newMinutes = prevState?.seconds < 59 ? prevState?.minutes : prevState.minutes + 1;
+                    setTimer(`${newMinutes >= 10 ? "" : "0"}${newMinutes}:${newSeconds >= 10 ? "" : "0"}${newSeconds}`);
+                    return { seconds: newSeconds, minutes: newMinutes };
+                });
+            }, 1000);
+        }
+        if (gameWon && intervalRef.current) { clearInterval(intervalRef.current) }
+    }
+    useEffect(() => {
+        gameTimer();
+    }, [timerRuns, gameWon])
+    useEffect(() => {
+        if (gameWon) {
+            setTimerRuns(false);
+        }
+    }, [gameWon]);
     useEffect(() => {
         setDice(generateAllDies());
         setLoading(false);
@@ -63,6 +90,7 @@ export default function Game() {
                     onClick={roleDice}>
                     {gameWon ? "New Game" : "Roll Dice"}
                 </button>
+                <h1 className="text-2xl font-bold">{timer}</h1>
             </div>
             <Score />
         </section>
